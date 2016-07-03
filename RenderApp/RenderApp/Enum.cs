12cc8c0afc87dd4107d70baab6ddcadf67f6c50c
slothgreed <stmndrm@gmail.com>
2016-07-04 00:04:38 +0900

    refactor enum

--- RenderApp/RenderApp/Enum.cs
+++ RenderApp/RenderApp/Enum.cs
@@ -6,6 +6,35 @@ using System.Threading.Tasks;
 
 namespace RenderApp
 {
+    /// <summary>
+    /// Attribute(Shader用)
+    /// </summary>
+    public enum EAttrib
+    {
+        Vertex = 0,
+        Normal = 1,
+        Color = 2,
+        Texture = 3,
+        Index = 4,
+        Timer = 5,
+        Num = 6,
+    }
+    /// <summary>
+    /// レンダリングステージ
+    /// </summary>
+    public enum ERenderingStage
+    {
+        GeometryPass,
+        LightingPass,
+        PostProcessPass,
+        ForwardPass
+    }
+    public enum TextureType
+    {
+        Texture2D,
+        FrameBuffer,
+        CubeMap
+    }
     public enum TextureKind
     {
         None = -1,
