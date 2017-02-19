d18e42a86776f43ae501d11462c3a1dbc0f5ecc1
slothgreed <stmndrm@gmail.com>
2017-02-19 23:11:17 +0900

    GBufferの変更

--- RenderApp/Render_System/GBuffer.cs
+++ RenderApp/Render_System/GBuffer.cs
@@ -6,32 +6,41 @@ using System.Threading.Tasks;
 using RenderApp.GLUtil;
 using KI.Gfx.KIAsset;
 using KI.Gfx.Render;
+using RenderApp.AssetModel;
 namespace RenderApp.Render_System
 {
-    public class GBuffer : RenderTarget
+    public class GBuffer : RenderTechnique
     {
         Dictionary<TextureKind, Texture> textureList = new Dictionary<TextureKind, Texture>();
 
         public GBuffer(int width, int height)
             :base("GBuffer")
         {
+        
+        
+        }
+
+        public override void CreateRenderTarget(int width, int height)
+        {
             Texture[] texture = new Texture[4];
             texture[0] = TextureFactory.Instance.CreateTexture("GPosit", width, height);
             texture[1] = TextureFactory.Instance.CreateTexture("GNormal", width, height);
             texture[2] = TextureFactory.Instance.CreateTexture("GColor", width, height);
             texture[3] = TextureFactory.Instance.CreateTexture("GLight", width, height);
-            Initialize(width, height, texture);
 
             RenderApp.Globals.Project.ActiveProject.AddChild(texture[0]);
             RenderApp.Globals.Project.ActiveProject.AddChild(texture[1]);
             RenderApp.Globals.Project.ActiveProject.AddChild(texture[2]);
             RenderApp.Globals.Project.ActiveProject.AddChild(texture[3]);
 
-            textureList.Add(TextureKind.World, Textures[0]);
-            textureList.Add(TextureKind.Normal, Textures[1]);
-            textureList.Add(TextureKind.Albedo, Textures[2]);
-            textureList.Add(TextureKind.Lighting, Textures[3]);
+            textureList.Add(TextureKind.World, texture[0]);
+            textureList.Add(TextureKind.Normal, texture[1]);
+            textureList.Add(TextureKind.Albedo, texture[2]);
+            textureList.Add(TextureKind.Lighting, texture[3]);
+
+            RenderTarget = new RenderTarget(Name, width, height, texture);
         }
+
         public Texture FindTexture(TextureKind kind)
         {
             if(textureList.ContainsKey(kind))
@@ -41,5 +50,24 @@ namespace RenderApp.Render_System
             return null;
         }
 
+
+        public override void Initialize()
+        {
+        }
+
+        public void Render()
+        {
+            ClearBuffer();
+            RenderTarget.BindRenderTarget();
+            foreach (var asset in SceneManager.Instance.ActiveScene.RootNode.AllChildren())
+            {
+                if (asset._KIObject is Geometry)
+                {
+                    var geometry = asset._KIObject as Geometry;
+                    geometry.Render();
+                }
+            }
+            RenderTarget.UnBindRenderTarget();
+        }
     }
 }
